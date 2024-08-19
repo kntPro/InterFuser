4df508c5bf08647de98b0742d46fd0f34416c3ed
kntpro <kentoyamamoto26@gmail.com>
2024-08-19 13:18:27 +0900

    setup and change carla version

--- setup_carla.sh
+++ setup_carla.sh
@@ -3,10 +3,12 @@
 # Download and install CARLA
 mkdir carla
 cd carla
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.10.1.tar.gz
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.10.1.tar.gz
-tar -xf CARLA_0.9.10.1.tar.gz
-tar -xf AdditionalMaps_0.9.10.1.tar.gz
+wget https://tiny.carla.org/carla-0-9-15-linux
+wget https://tiny.carla.org/additional-maps-0-9-15-linux
+tar -zxvf CARLA_0.9.15.tar.gz
+mv ./AdditionalMaps_0.9.15.tar.gz Import/
+chmod 777 ImportAssets.sh
+./ImportAssets.sh
 rm CARLA_0.9.10.1.tar.gz
-rm AdditionalMaps_0.9.10.1.tar.gz
+rm Import/AdditionalMaps_0.9.10.1.tar.gz
 cd ..
